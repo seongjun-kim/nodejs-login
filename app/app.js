11d16aebbe457e6bf3f455bb00038f26b3de3f18
seongjun_kim <lewisel@naver.com>
2021-07-14 21:04:05 +0900

    프론트엔드 전용 js 파일 만들고 연동

--- app/app.js
+++ app/app.js
@@ -9,7 +9,10 @@ const home = require("./src/routes/home");
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+
+// 미들웨어
 // use: 미들웨어(module)를 등록해주는 메서드
 app.use("/", home);
+app.use(express.static(`${__dirname}/src/public`));
 
 module.exports = app;
