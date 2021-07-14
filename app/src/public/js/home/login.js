10311766ea89c7ea2c03874d908f86611f7f231f
seongjun_kim <lewisel@naver.com>
2021-07-14 21:22:09 +0900

    로그인 버튼 기능 연동

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("hello");
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginButton = document.querySelector("button");
+
+loginButton.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+    alert(`id: ${req.id}, pw: ${req.password}`);
+}
