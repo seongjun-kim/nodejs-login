1e8ee4bd50572b5a4cb16be1fc5ed407d9206d86
seongjun_kim <lewisel@naver.com>
2021-07-14 20:55:35 +0900

    폴더 구조 변경에 따른 경로 수정

--- app/app.js
+++ app/app.js
@@ -5,9 +5,9 @@ const express = require("express");
 const app = express();
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 // use: 미들웨어(module)를 등록해주는 메서드
 app.use("/", home);
