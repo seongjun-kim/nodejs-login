68b0736ca43c4c1345b25f2e60e9aaafce818956
seongjun_kim <lewisel@naver.com>
2021-07-15 11:22:07 +0900

    'public 폴더-static',  'script 태그 defer 속성' 활용 이유

--- app/app.js
+++ app/app.js
@@ -16,6 +16,6 @@ app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // URL 통해 전달되는 데이터에 한글/공백 등의 문자도 정상 인식되도록
 app.use("/", home);
-app.use(express.static(`${__dirname}/src/public`));
+app.use(express.static(`${__dirname}/src/public`)); // 이미지/CSS/JS 파일과 같은 '정적 파일을 활용하기 위해' Express 기본 제공 미들웨어 함수(static) 사용
 
 module.exports = app;
